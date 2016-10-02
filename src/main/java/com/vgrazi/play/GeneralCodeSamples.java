946ad7606b90dca464e2439c83caa92480959bba
Victor J Grazi <vgrazi@gmail.com>
2016-10-02 13:42:46 -0400

    Now using latest release number 1.1.10 instead of RELEASE

--- src/main/java/com/vgrazi/play/GeneralCodeSamples.java
+++ src/main/java/com/vgrazi/play/GeneralCodeSamples.java
@@ -656,7 +656,7 @@ public class GeneralCodeSamples {
 
   @Test
   public void testCompletable() {
-    Completable.create(s -> System.out.println(s));
+    Completable.create((Completable.OnSubscribe) s -> System.out.println(s));
   }
 
   private static void sleep(long value) {
